d6ecc984adbfd97e2146ecc544d346483614206c
chicainoportuna <paula.mara@gmail.com>
2018-12-06 15:19:46 +0100

    Declare variables and add listener to button

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-'use strict'
+'use strict';
+
+let nickname = document.querySelector('.nickname'); // Nickname que introducimos
+let button = document.querySelector('.btn'); // Botón para bucar el nombre
+let results = document.querySelector('.results'); // Lista de letras del nombre
+
+function giveMeTheName()
+
+button.addEventListener('click', giveMeTheName);
